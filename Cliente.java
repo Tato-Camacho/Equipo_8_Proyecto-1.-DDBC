6708ee440ab21bad67816b765bf0362834908fa2
Gabriel Camacho <81324220+Tato-Camacho@users.noreply.github.com>
2022-05-11 09:39:36 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,21 +1,31 @@
+// La clase Cliente sirve para 
+
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
+
+// Cliente Inicializa la IP dependiendo de donde queramos entrar y el puerto
 public class Cliente {
 	private static  String IP = ""; // Puedes cambiar a localhost
 	private static  int PUERTO = 1100; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
-    public static void main(String[] args) throws RemoteException, NotBoundException {
+
+// Main es el arreglo de cadenas que va a traer el Local Host mas otras 2 IP	
+	public static void main(String[] args) throws RemoteException, NotBoundException {
         Scanner sc = new Scanner(System.in);
         int eleccion;
         float numero1, numero2, resultado = 0;
 		String[] ips={"localhost","192.168.1.208", "192.168.1.103"};
 		int i=0;
+//El arreglo de cargas es el que se encarga de ordenar la carga de los servidores y sus IP's
 		int[] cargas= new int[3];
 		int imin=0;
         do {
 			imin=0;
+		
+// Este ciclo obtiene todas las cargas y las va guardando en su espacio correspondiente con la funcion getCarga
+//Tambien se compara cual carga es la mas pequeña para guardar en imin
 			for(i=0;i<=2;i++){
 				PUERTO = 1100;
 				Registry registry = LocateRegistry.getRegistry(ips[i], PUERTO);
@@ -35,6 +45,8 @@ public class Cliente {
 				imin=2;
 				System.out.println("Servidor 192.168.1.103");
 			}
+
+// 
 			IP = ips[imin];
 			PUERTO = 1100;
 			Registry registry = LocateRegistry.getRegistry(IP, PUERTO);
